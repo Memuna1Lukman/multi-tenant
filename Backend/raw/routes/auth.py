a68352180dee8f9687910402f05f503be0eb2563
Memuna1Lukman <memunalukman82@gmail.com>
2026-08-17 23:24:55 +0000

    ready to debug

--- Backend/raw/routes/auth.py
+++ Backend/raw/routes/auth.py
@@ -14,16 +14,16 @@ router = APIRouter(
 def login_user(
     response:Response,
     user:OAuth2PasswordRequestForm=Depends(),db:Session = Depends(get_db)):
-    check_user = db.query(models.User).filter(models.User.email == user.username).first()
+    check_user = db.query(models.Users).filter(models.Users.email == user.username).first()
     if not check_user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Invalid Credentials")
 
-    verify_password = utils.unhash_password(user.password,check_user.password)
+    verify_password = utils.verify_password(user.password,check_user.password_hash)
     if not verify_password:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Invalid Credentials")
 
     
-    access_token = oauth2.create_token(data={"owner_id": check_user.id})
+    access_token = oauth.create_token(data={"owner_id": check_user.id})
     # reponse in the cookie form
     response.set_cookie(
         key="access_token",
